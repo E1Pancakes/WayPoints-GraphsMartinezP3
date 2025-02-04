32f7d93907e403eda111384683a2a1c3afcc6dfe
Diego Martinez <145380966+E1Pancakes@users.noreply.github.com>
2025-02-04 11:20:33 -0800

    fixed slerp

--- WayPoints-GraphsDMarintezP3/Assets/followWP.cs
+++ WayPoints-GraphsDMarintezP3/Assets/followWP.cs
@@ -41,15 +41,22 @@ public class followWP : MonoBehaviour
     {
         if (g.pathList.Count == 0 || currentWP == g.pathList.Count)
             return;
-        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position,this.transform.position) < accuracy)
+        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position,
+                            this.transform.position) < accuracy)
         {
+            currentNode = g.pathList[currentWP].getId();
             currentWP++;
         }
 
         if (currentWP < g.pathList.Count)
         {
             goal = g.pathList[currentWP].getId().transform;
-            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 lookAtGoal = new Vector3(goal.position.x, 
+                                                 this.transform.position.y,
+                                               goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
         }
     }
 }
